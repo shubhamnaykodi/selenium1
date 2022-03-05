711f6c84d90563fe9bdeff1108010a8abf0d83f2
sdnay <sdnay@DESKTOP-MKC96IA>
2022-03-05 14:41:19 +0530

    first commit

--- src/Hello_Github.java
+++ src/Hello_Github.java
@@ -3,12 +3,6 @@ public class Hello_Github {
 	public static void main(String[] args) {
 		System.out.println("hii from eclipse");
 		//hi from github
-		int a=2;
-		if(a%2==0) {
-			System.out.println("even");
-		}
-		else{
-				System.out.println("odd");
-		}
+		
 	}
 }
